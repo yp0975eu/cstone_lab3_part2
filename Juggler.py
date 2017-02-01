61b74c399ba2f8f515e812c242b89455212883ef
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-02-01 11:03:13 -0600

    created new juggler class and adds sqlalchemy import statement

---
+++ Juggler.py
@@ -0,0 +1,5 @@
+class Juggler:
+    def __init__(self, name, country, catches):
+        self.name = name
+        self.country = country
+        self.catches = catches
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import logging
+import sqlalchemy
 
 
 class DB:
